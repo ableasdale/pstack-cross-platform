258a00f9922e55fea6a756518694f3959f4d31e7
Alex Bleasdale <ableasdale@marklogic.com>
2016-08-28 10:14:38 +0100

    Initial cross platform work now complete

--- new-linux-pstack.sh
+++ new-linux-pstack.sh
@@ -10,16 +10,41 @@ reset=`tput sgr0`
 
 function box_out()
 {
-  local s=("$@") b w
-  for l in "${s[@]}"; do
+    local s=("$@") b w
+    for l in "${s[@]}"; do
     ((w<${#l})) && { b="$l"; w="${#l}"; }
-  done
-  echo -e "${yellow}╔═${b//?/═}═╗\n║ ${b//?/ } ║"
-  for l in "${s[@]}"; do
+    done
+    echo -e "${yellow}╔═${b//?/═}═╗\n║ ${b//?/ } ║"
+    for l in "${s[@]}"; do
     printf '║ %s%*s%s ║\n' "${blue}" "-$w" "$l" "${yellow}"
-  done
-  echo -e "║ ${b//?/ } ║\n╚═${b//?/═}═╝"
-  tput sgr 0
+    done
+    echo -e "║ ${b//?/ } ║\n╚═${b//?/═}═╝"
+    tput sgr 0
+}
+
+function sar_collect()
+{
+    # SAR stats for host
+    echo "CPU statistics [sar -u ALL]" >> /tmp/$TSTAMP/sar-summary.log
+    sar -u ALL >> /tmp/$TSTAMP/sar-summary.log
+    echo "CPU individual core statistics [sar -P ALL]" >> /tmp/$TSTAMP/sar-summary.log
+    sar -P ALL >> /tmp/$TSTAMP/sar-summary.log
+    echo "Block device statistics [sar -b]" >> /tmp/$TSTAMP/sar-summary.log
+    sar -b >> /tmp/$TSTAMP/sar-summary.log
+    echo "Paging activity [sar -B]" >> /tmp/$TSTAMP/sar-summary.log
+    sar -B >> /tmp/$TSTAMP/sar-summary.log
+    echo "Swap statistics [sar -S]" >> /tmp/$TSTAMP/sar-summary.log
+    sar -S >> /tmp/$TSTAMP/sar-summary.log
+    echo "Memory use statistics [sar -r]" >> /tmp/$TSTAMP/sar-summary.log
+    sar -r >> /tmp/$TSTAMP/sar-summary.log
+    echo "Device statistics [sar -d]" >> /tmp/$TSTAMP/sar-summary.log
+    sar -d >> /tmp/$TSTAMP/sar-summary.log
+    echo "Process switching [sar -w]" >> /tmp/$TSTAMP/sar-summary.log
+    sar -w >> /tmp/$TSTAMP/sar-summary.log
+    echo "Process queues [sar -q]" >> /tmp/$TSTAMP/sar-summary.log
+    sar -q >> /tmp/$TSTAMP/sar-summary.log
+    echo "Network statistics [sar -n ALL]" >> /tmp/$TSTAMP/sar-summary.log
+    sar -n ALL >> /tmp/$TSTAMP/sar-summary.log
 }
 
 # root check
@@ -59,56 +84,67 @@ then
     iostat 2 25 >> /tmp/$TSTAMP/iostat.log &
     vmstat 2 25 >> /tmp/$TSTAMP/vmstat.log &
     service MarkLogic pmap >> /tmp/$TSTAMP/pmap.log
+    while [ $TIME -gt 0 ]; do
+        service MarkLogic pstack | tee -a /tmp/$TSTAMP/pstack.log | awk 'BEGIN { s = ""; } /^Thread/ { print s; s = ""; } /^\#/ { if (s != "" ) { s = s "," $4} else { s = $4 } } END { print s }' | sort | uniq -c | sort -r -n -k 1,1 >> /tmp/$TSTAMP/pstack-summary.log
+        # OR you can use gdb -ex "set pagination 0" -ex "thread apply all bt" -batch -p $PID
+        sleep $INTERVAL
+        echo -e ". \c"
+        let TIME-=$INTERVAL
+    done
+    date | tee -a >> /tmp/$TSTAMP/pmap.log
+    service MarkLogic pmap >> /tmp/$TSTAMP/pmap.log
+    sar_collect
 else
+    # For now - we'll just assume it's not linux, so it's OS X :S
+    # TODO - elif darwin? else "not supported"
     # OS X: vm_stat -c 25 2 iostat -c 25 2
+    hostinfo >> /tmp/$TSTAMP/hostinfo.log
     iostat -c 25 2 >> /tmp/$TSTAMP/iostat.log &
     vm_stat -c 25 2 >> /tmp/$TSTAMP/vmstat.log &
-    # TODO - pmap for PSX?
+    # pmap for OS X:
+    vmmap $PID >> /tmp/$TSTAMP/pmap.log
+    while [ $TIME -gt 0 ]; do
+        lldb -o "thread backtrace all" --batch -p $PID | tee -a /tmp/$TSTAMP/pstack.log | awk 'BEGIN { s = ""; } /^Thread/ { print s; s = ""; } /^\#/ { if (s != "" ) { s = s "," $4} else { s = $4 } } END { print s }' | sort | uniq -c | sort -r -n -k 1,1 >> /tmp/$TSTAMP/pstack-summary.log
+        # TODO - OR service MarkLogic pstack?  Maybe check for presence of pstack??
+        sleep $INTERVAL
+        echo -e ". \c"
+        let TIME-=$INTERVAL
+    done
+    date | tee -a >> /tmp/$TSTAMP/pmap.log
+    vmmap $PID >> /tmp/$TSTAMP/pmap.log
+    # TODO - sar on OSX - it needs to be set up and tested on my mac...
 fi
 
 # for s in ${PIDS[@]}; do
 
 # PStacks for MarkLogic process
-while [ $TIME -gt 0 ]; do
+#while [ $TIME -gt 0 ]; do
+#
+#    date | tee -a /tmp/$TSTAMP/pstack.log >> /tmp/$TSTAMP/pstack-summary.log
+#    if [[ $OSTYPE == linux-gnu* ]]
+#    then
+#        service MarkLogic pstack | tee -a /tmp/$TSTAMP/pstack.log | awk 'BEGIN { s = ""; } /^Thread/ { print s; s = ""; } /^\#/ { if (s != "" ) { s = s "," $4} else { s = $4 } } END { print s }' | sort | uniq -c | sort -r -n -k 1,1 >> /tmp/$TSTAMP/pstack-summary.log
+#        # TODO - OR gdb -ex "set pagination 0" -ex "thread apply all bt" -batch -p $PID
+#    else
+#        lldb -o "thread backtrace all" --batch -p $PID | tee -a /tmp/$TSTAMP/pstack.log | awk 'BEGIN { s = ""; } /^Thread/ { print s; s = ""; } /^\#/ { if (s != "" ) { s = s "," $4} else { s = $4 } } END { print s }' | sort | uniq -c | sort -r -n -k 1,1 >> /tmp/$TSTAMP/pstack-summary.log
+#    fi
+#
+#	#pause and update stdout to show some progress
+#    sleep $INTERVAL
+#    echo -e ". \c"
+#    let TIME-=$INTERVAL
+#done
+## PMAP For MarkLogic process
+#date | tee -a >> /tmp/$TSTAMP/pmap.log
+#service MarkLogic pmap >> /tmp/$TSTAMP/pmap.log
 
-    date | tee -a /tmp/$TSTAMP/pstack.log >> /tmp/$TSTAMP/pstack-summary.log
-    if [[ $OSTYPE == linux-gnu* ]]
-    then
-        gdb -ex "set pagination 0" -ex "thread apply all bt" -batch -p $PID | tee -a /tmp/$TSTAMP/pstack.log | awk 'BEGIN { s = ""; } /^Thread/ { print s; s = ""; } /^\#/ { if (s != "" ) { s = s "," $4} else { s = $4 } } END { print s }' | sort | uniq -c | sort -r -n -k 1,1 >> /tmp/$TSTAMP/pstack-summary.log
-        # TODO - OR service MarkLogic pstack?  Maybe check for presence of pstack??
-    else
-        lldb -o "thread backtrace all" --batch -p $PID | tee -a /tmp/$TSTAMP/pstack.log | awk 'BEGIN { s = ""; } /^Thread/ { print s; s = ""; } /^\#/ { if (s != "" ) { s = s "," $4} else { s = $4 } } END { print s }' | sort | uniq -c | sort -r -n -k 1,1 >> /tmp/$TSTAMP/pstack-summary.log
-    fi
+#if hash gdate 2>/dev/null; then
+#    gdate "$@"
+#else
+#    date "$@"
+#fi
 
-	#pause and update stdout to show some progress
-    sleep $INTERVAL
-    echo -e ". \c"
-    let TIME-=$INTERVAL
-done
-# PMAP For MarkLogic process
-date | tee -a >> /tmp/$TSTAMP/pmap.log
-service MarkLogic pmap >> /tmp/$TSTAMP/pmap.log
-# SAR stats for host
-echo "CPU statistics [sar -u ALL]" >> /tmp/$TSTAMP/sar-summary.log
-sar -u ALL >> /tmp/$TSTAMP/sar-summary.log
-echo "CPU individual core statistics [sar -P ALL]" >> /tmp/$TSTAMP/sar-summary.log
-sar -P ALL >> /tmp/$TSTAMP/sar-summary.log
-echo "Block device statistics [sar -b]" >> /tmp/$TSTAMP/sar-summary.log
-sar -b >> /tmp/$TSTAMP/sar-summary.log
-echo "Paging activity [sar -B]" >> /tmp/$TSTAMP/sar-summary.log
-sar -B >> /tmp/$TSTAMP/sar-summary.log
-echo "Swap statistics [sar -S]" >> /tmp/$TSTAMP/sar-summary.log
-sar -S >> /tmp/$TSTAMP/sar-summary.log
-echo "Memory use statistics [sar -r]" >> /tmp/$TSTAMP/sar-summary.log
-sar -r >> /tmp/$TSTAMP/sar-summary.log
-echo "Device statistics [sar -d]" >> /tmp/$TSTAMP/sar-summary.log
-sar -d >> /tmp/$TSTAMP/sar-summary.log
-echo "Process switching [sar -w]" >> /tmp/$TSTAMP/sar-summary.log
-sar -w >> /tmp/$TSTAMP/sar-summary.log
-echo "Process queues [sar -q]" >> /tmp/$TSTAMP/sar-summary.log
-sar -q >> /tmp/$TSTAMP/sar-summary.log
-echo "Network statistics [sar -n ALL]" >> /tmp/$TSTAMP/sar-summary.log
-sar -n ALL >> /tmp/$TSTAMP/sar-summary.log
+
 echo completed
 echo `date`
 # create zip
