baf1e75b2b7b14b3a99b8102226b88e19a189d8f
Alex Bleasdale <ableasdale@marklogic.com>
2016-08-29 07:22:02 +0100

    Added some sar collection data for OSX

--- new-linux-pstack.sh
+++ new-linux-pstack.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
+# global vars
+TSTAMP=`date +"%H%M%S-%m-%d-%Y"`
+INTERVAL=5
+TIME=${1:-180}
+PIDS=(`pidof MarkLogic`)
+PID=${PIDS[1]}
+
 red=`tput setaf 1`
 green=`tput setaf 2`
 yellow=`tput setaf 3`
 blue=`tput setaf 4`
 reset=`tput sgr0`
-
-# pidof MarkLogic returns 2 pids - figure out the correct one...
 
 function box_out()
 {
@@ -24,27 +29,34 @@ function box_out()
 
 function sar_collect()
 {
-    # SAR stats for host
-    echo "CPU statistics [sar -u ALL]" >> /tmp/$TSTAMP/sar-summary.log
-    sar -u ALL >> /tmp/$TSTAMP/sar-summary.log
-    echo "CPU individual core statistics [sar -P ALL]" >> /tmp/$TSTAMP/sar-summary.log
-    sar -P ALL >> /tmp/$TSTAMP/sar-summary.log
-    echo "Block device statistics [sar -b]" >> /tmp/$TSTAMP/sar-summary.log
-    sar -b >> /tmp/$TSTAMP/sar-summary.log
-    echo "Paging activity [sar -B]" >> /tmp/$TSTAMP/sar-summary.log
-    sar -B >> /tmp/$TSTAMP/sar-summary.log
-    echo "Swap statistics [sar -S]" >> /tmp/$TSTAMP/sar-summary.log
-    sar -S >> /tmp/$TSTAMP/sar-summary.log
-    echo "Memory use statistics [sar -r]" >> /tmp/$TSTAMP/sar-summary.log
-    sar -r >> /tmp/$TSTAMP/sar-summary.log
-    echo "Device statistics [sar -d]" >> /tmp/$TSTAMP/sar-summary.log
-    sar -d >> /tmp/$TSTAMP/sar-summary.log
-    echo "Process switching [sar -w]" >> /tmp/$TSTAMP/sar-summary.log
-    sar -w >> /tmp/$TSTAMP/sar-summary.log
-    echo "Process queues [sar -q]" >> /tmp/$TSTAMP/sar-summary.log
-    sar -q >> /tmp/$TSTAMP/sar-summary.log
-    echo "Network statistics [sar -n ALL]" >> /tmp/$TSTAMP/sar-summary.log
-    sar -n ALL >> /tmp/$TSTAMP/sar-summary.log
+    if [[ $OSTYPE == linux-gnu* ]]
+    then
+        # SAR stats for host (Linux)
+        echo "CPU statistics [sar -u ALL]" >> /tmp/$TSTAMP/sar-summary.log
+        sar -u ALL >> /tmp/$TSTAMP/sar-summary.log
+        echo "CPU individual core statistics [sar -P ALL]" >> /tmp/$TSTAMP/sar-summary.log
+        sar -P ALL >> /tmp/$TSTAMP/sar-summary.log
+        echo "Block device statistics [sar -b]" >> /tmp/$TSTAMP/sar-summary.log
+        sar -b >> /tmp/$TSTAMP/sar-summary.log
+        echo "Paging activity [sar -B]" >> /tmp/$TSTAMP/sar-summary.log
+        sar -B >> /tmp/$TSTAMP/sar-summary.log
+        echo "Swap statistics [sar -S]" >> /tmp/$TSTAMP/sar-summary.log
+        sar -S >> /tmp/$TSTAMP/sar-summary.log
+        echo "Memory use statistics [sar -r]" >> /tmp/$TSTAMP/sar-summary.log
+        sar -r >> /tmp/$TSTAMP/sar-summary.log
+        echo "Device statistics [sar -d]" >> /tmp/$TSTAMP/sar-summary.log
+        sar -d >> /tmp/$TSTAMP/sar-summary.log
+        echo "Process switching [sar -w]" >> /tmp/$TSTAMP/sar-summary.log
+        sar -w >> /tmp/$TSTAMP/sar-summary.log
+        echo "Process queues [sar -q]" >> /tmp/$TSTAMP/sar-summary.log
+        sar -q >> /tmp/$TSTAMP/sar-summary.log
+        echo "Network statistics [sar -n ALL]" >> /tmp/$TSTAMP/sar-summary.log
+        sar -n ALL >> /tmp/$TSTAMP/sar-summary.log
+    else
+        # OSX Sar
+        sar -A -o /tmp/$TSTAMP/sar-summary.log
+    fi
+
 }
 
 # root check
@@ -57,13 +69,6 @@ fi
 if [[ -z $1 ]]; then
     box_out 'Usage: ./ml-support-dump.sh [running time in seconds]' 'This script will run for the default 180 seconds' 'Please ensure sar is configured correctly'
 fi
-
-# global vars
-TSTAMP=`date +"%H%M%S-%m-%d-%Y"`
-INTERVAL=5
-TIME=${1:-180}
-PIDS=(`pidof MarkLogic`)
-PID=${PIDS[1]}
 
 # main
 echo pstack script started at: ${green}`date`${reset} - running for \(approximately\) ${green}$TIME${reset} seconds. MarkLogic pid is ${green}$PID${reset}
